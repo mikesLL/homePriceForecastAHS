3266a893119b47a250910edda718f5f4b89adfa2
Michael Sharifi <michael.a.sharifi@gmail.com>
2016-03-23 18:23:34 -0700

    mods to gen_port

--- gen_weights.m
+++ gen_weights.m
@@ -11,9 +11,10 @@ port_ds should include:
 sharpe_ratio, util, port_ret, x_opt_store
 %}
 
-function [ port_ds ] = gen_weights(param, city_id, ds_use, y_ds, micro_flag, mu_h_flag, mv_gamma )
+function [ port_ds ] = gen_weights(param, city_id, ds_use, y_ds, mu_h_flag, mv_gamma )
 
 save('gen_weights_save');
+
 %%
 port_ds = dataset;
 port_ds.sharpe_ratio = zeros(100,1);
@@ -24,7 +25,6 @@ port_ds.valid = zeros(100,1);
 
 y_1f_combo_var = zeros(100,1);
 x_opt_store = zeros( length(y_ds) ,4);
-risk_weight = zeros(length(y_ds), 1 );
 rf = .01;
 i_combo_use = 4;
 
@@ -40,21 +40,14 @@ t_end = find(y_ds.valid,1,'last');
 
 h_step = 4;
 %%
-%{
-y_1f_combo: y_ds.fore_combo
-%}
-%%
 for t_use = (t_begin + h_step + 1) : t_end
     port_ds.valid(t_use) = 1;
-    %t_est = t_use - h_step;
+   
     y_1f_combo_var(t_use, 1) = ...
         var( y_ds.RET_fut(t_begin + h_step:t_use) - y_ds.fore_combo(t_begin + h_step:t_use,i_combo_use) - .5*X_apr(t_begin + h_step:t_use ) );
     
-    %y_1f_var_est = y_ds.RET_fut(1:t_use);
-    %spy_var_est = X_spy_ret(1:t_use);
     y_1f_var_est = ( y_ds.RET_fut(t_begin:t_use) - y_ds.fore_combo(t_begin:t_use, i_combo_use) );
     spy_var_est = X_spy_ret(t_begin:t_use);
-    
     
     OMEGA_tmp = cov([spy_var_est, y_1f_var_est]); 
     OMEGA = zeros(4,4);
@@ -63,7 +56,6 @@ for t_use = (t_begin + h_step + 1) : t_end
     mu_x = .065; % + X_spy_yield(t_use);
     t_cost = .00;
     
-    %mu_h = .95*y_1f_combo(t_use,i_combo_use) + .9*X_rp(t_use);    
     if ( mu_h_flag == 0 )
         mu_h = 0;
     else
@@ -78,7 +70,6 @@ for t_use = (t_begin + h_step + 1) : t_end
     x_opt_store(t_use,:) = x_opt;
     disp(x_opt);
     port_ds.x_opt(t_use,:) = x_opt;
-    %ret_exante = [X_spy_ret_fut(t_use) + X_spy_yield(t_use), y_city(t_use) + X_rp(t_use) - .15, .7*X_apr(t_use), .5*X_apr(t_use)    ];
     ret_exante = [X_spy_ret_fut(t_use) + X_spy_yield(t_use), y_ds.RET_fut(t_use) + X_rp(t_use) , .7*X_apr(t_use), .5*X_apr(t_use)    ];
 
     port_ds.port_ret(t_use) = x_opt * ret_exante';
@@ -86,13 +77,12 @@ for t_use = (t_begin + h_step + 1) : t_end
     idx_use2 = (port_ds.valid == 1);
     port_ds.sharpe_ratio(t_use) = mean( port_ds.port_ret(idx_use2) ) /  std( port_ds.port_ret(idx_use2) ) ;
     port_ds.util(t_use) = mean( port_ds.port_ret(idx_use2) ) - mv_gamma / 2.0 * var( port_ds.port_ret(idx_use2) ) ;
-   
 end
-
-
 
 end
 
+%ret_exante = [X_spy_ret_fut(t_use) + X_spy_yield(t_use), y_city(t_use) + X_rp(t_use) - .15, .7*X_apr(t_use), .5*X_apr(t_use)    ];
+%mu_h = .95*y_1f_combo(t_use,i_combo_use) + .9*X_rp(t_use); 
 %sharpe_ratio = mean( port_ret(45:t_end) ) / ( std( port_ret(45:t_end) )  );
 %util = mean( port_ret(45:t_end) ) - mv_gamma / 2 *  var( port_ret(45:t_end) ) ;
 
