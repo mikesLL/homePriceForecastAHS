9ba7a1af380094d06c6533a025f33bd7ce66a288
Michael Sharifi <michael.a.sharifi@gmail.com>
2016-03-24 18:10:47 -0700

    mods to tables

--- gen_port.m
+++ gen_port.m
@@ -16,16 +16,19 @@ save('gen_weights_save');
 
 %%
 port_ds = dataset;
-port_ds.sharpe_ratio = zeros(100,1);
-port_ds.util = zeros(100,1);
-port_ds.port_ret = zeros(100,1);
-port_ds.x_opt = zeros(100,4);
-port_ds.valid = zeros(100,1);
+port_ds.mean_est = zeros(length(y_ds),1);
+port_ds.std_est = zeros(length(y_ds),1);
+port_ds.sharpe_ratio = zeros(length(y_ds),1);
+port_ds.sharpe_ratio = zeros(length(y_ds),1);
+port_ds.util = zeros(length(y_ds),1);
+port_ds.port_ret = zeros(length(y_ds),1);
+port_ds.x_opt = zeros(length(y_ds),4);
+port_ds.valid = zeros(length(y_ds),1);
 
 rf = .01;
 i_combo_use = 4;
 
-idx_use = all ([ ds_use.city_id == city_id, ds_use.YEAR >= 1988 , ds_use.YEAR <= 2012 ] ,2 );
+idx_use = all ([ ds_use.city_id == city_id, ds_use.YEAR >= param.year_beg , ds_use.YEAR <= param.year_end ] ,2 );
 X_apr = ds_use.APR(idx_use);
 X_rp = ds_use.RP(idx_use);
 X_spy_ret = ds_use.spy_ret(idx_use);
@@ -60,7 +63,6 @@ for t_use = (t_begin + h_step + 1) : t_end
     MU = [mu_x mu_h mu_apr mu_rf ]';
     
     port_ds.x_opt(t_use,:) = gen_MVw( MU, OMEGA, mv_gamma );
-    %disp( port_ds.x_opt(t_use,:) );
     
     ret_exante = [X_spy_ret_fut(t_use) + X_spy_yield(t_use), ...
         y_ds.RET_fut(t_use) + X_rp(t_use) , ...
@@ -69,7 +71,12 @@ for t_use = (t_begin + h_step + 1) : t_end
     port_ds.port_ret(t_use) = port_ds.x_opt(t_use,:) * ret_exante';
     
     idx_use2 = (port_ds.valid == 1);
-    port_ds.sharpe_ratio(t_use) = mean( port_ds.port_ret(idx_use2) ) /  std( port_ds.port_ret(idx_use2) ) ;
+    port_ds.mean_est(t_use) = mean( port_ds.port_ret(idx_use2) );
+    port_ds.std_est(t_use) = std( port_ds.port_ret(idx_use2) );
+    port_ds.sharpe_ratio(t_use) = port_ds.mean_est(t_use) / port_ds.std_est(t_use);
+    port_ds.util(t_use) = port_ds.mean_est(t_use) - mv_gamma / 2.0 * port_ds.std_est(t_use).^2;
+
+    %port_ds.sharpe_ratio(t_use) = mean( port_ds.port_ret(idx_use2) ) /  std( port_ds.port_ret(idx_use2) ) ;
     port_ds.util(t_use) = mean( port_ds.port_ret(idx_use2) ) - mv_gamma / 2.0 * var( port_ds.port_ret(idx_use2) ) ;
 end
 
--- main.m
+++ main.m
@@ -4,7 +4,6 @@ loads data using fetch_data quart and then for each city computes h-step
 forecast and for each forecast computes OOS RMSE
 Then incorporates forecast into an mean-variance optimization problem and
 evaluates performance
-
 %}
 clear all
 
@@ -19,16 +18,12 @@ c = fred('https://research.stlouisfed.org/fred2/');     % connection to FRED Dat
 fromdate = '01/01/1986';   % beginning of date range for historical data
 todate = '01/01/2014';     % ending of date range for historical data
 
-if false
-    fetch_data;   %fetch_data_quart;   %fetch_data;
+if true
+    %% fetch data
+    ds_use = fetch_data_fn( param, dsreadin_codes, dsreadin_macro_data, fromdate, todate, c);
 else
     load fetch_data_save;
 end
-
-ds_use = ds_pool;
-
-%% fetch data
-ds_use = fetch_data_fn( param, dsreadin_codes, dsreadin_macro_data, fromdate, todate, c);
 
 %% generate the risk indices
 ds_use.risk_idx = zeros(length(ds_use),1);
@@ -39,10 +34,10 @@ ds_use = gen_risk_idx_fn( param, dsreadin_codes, ds_use, newhouse_flat );
 N_cities = max(ds_use.city_id);
 N_stats = 40;                       % max number of stats computed
 
-% table2: RMSE w/ micro vars
-% table3: RMSE w/ micro vars (normalized)
-% table2: RMSE
+% table2: RMSE 
 % table3: RMSE (normalized)
+% table2_mf: RMSE w/ micro vars
+% table3_mf: RMSE (normalized) w/ micro vars
 table2 = dataset( zeros( N_stats,1 ), 'VarNames', 'city1' );
 table2 = repmat(table2,1,N_cities);
 table2.Properties.VarNames = dsreadin_codes.city_str';
@@ -50,21 +45,13 @@ table3 = table2;
 table2_mf = table2;
 table3_mf = table2;
 
-table44 = dataset;
-table44.mu0 = zeros(N_cities,1);
-table44.std0 = zeros(N_cities,1);
-table44.mu1 = zeros(N_cities,1);
-table44.std1 = zeros(N_cities,1);
-table44.cer = zeros(N_cities,1);
-table44_mvgamma{20} = table44.cer;
-
-% table5: mu, std for portfolio with and without micro vars
-table5 = dataset( zeros(N_cities,1) );
-table5 = repmat(table5, 1, 5);
-table5.Properties.VarNames = {'mu0', 'std0', 'mu1', 'std1', 'cer' };
+% table4: mu, std for portfolio with and without micro vars
+table4 = dataset( zeros(N_cities,1) );
+table4 = repmat(table4, 1, 5);
+table4.Properties.VarNames = {'mu0', 'std0', 'mu1', 'std1', 'cer' };
 
 % table5_mvgamma: stores entire table for each gamma value
-table5_mvgamma{20} = table5;
+table4_mvgamma{20} = table4;
 
 %%
 city_id = 2;
@@ -73,28 +60,10 @@ gen_plot(param, ds_use, city_id, 'LAX');
 util_diff = zeros(N_cities,1);
 util0_store = zeros(N_cities,1);
 util1_store = zeros(N_cities,1);
-
-port_ret_store0{17} = zeros(100,1);
-port_ret_store1{17} = zeros(100,1);
-port_ret_store2{17} = zeros(100,1);
-
-X_opt_store0{17} = zeros(100,4);
-X_opt_store1{17} = zeros(100,4);
-X_opt_store2{17} = zeros(100,4);
-
 mv_gamma = 4;
 
 %%
-% want to be able to get rid of thse soon
-N_pred = 14;            % number of predictor variables w/ microdata
-N_pred0 = N_pred - 2;   % number of predictor variables w/o microdata
-N_naive = 2;            % number naive forecasts
-N_combo = 5;            % number combination forecasts
-N_tot = N_pred + N_naive + N_combo; % number all forecasts w/microdata
-N_tot0 = N_tot - 2;
-
 save('main_mid1_save');
-
 y_ds_store{N_cities} = dataset;
 y_ds_mf_store{N_cities} = dataset;
 
@@ -118,13 +87,52 @@ for city_id = 1:N_cities
     port_ds = gen_port(param, city_id, ds_use, y_ds, mu_h_flag, mv_gamma );
     port_ds_mf = gen_port(param, city_id, ds_use, y_ds_mf, mu_h_flag, mv_gamma );
     
+    table4.mu0(city_id) = port_ds.mean_est(96);
+    table4.std0(city_id) = port_ds.std_est(96);
+    table4.mu1(city_id) = port_ds_mf.mean_est(96);
+    table4.std1(city_id) = port_ds_mf.std_est(96);
+    v0 = table4.mu0(city_id)-mv_gamma/2.0*(table4.std0(city_id).^2);
+    v1 = table4.mu1(city_id)-mv_gamma/2.0*(table4.std1(city_id).^2);
+    table4.cer(city_id) = v1 - v0;
+    
     util0 = port_ds.util(96);
     util1 = port_ds_mf.util(96);
     disp( [util0 util1] );
     util0_store( city_id ) = util0;
     util1_store( city_id ) = util1;
     util_diff( city_id ) = util1 - util0;
+
 end
 
 save('main_save');
 
+%%
+%{
+port_ret_store0{17} = zeros(100,1);
+port_ret_store1{17} = zeros(100,1);
+port_ret_store2{17} = zeros(100,1);
+
+X_opt_store0{17} = zeros(100,4);
+X_opt_store1{17} = zeros(100,4);
+X_opt_store2{17} = zeros(100,4);
+%}
+
+%{
+table44 = dataset;
+table44.mu0 = zeros(N_cities,1);
+table44.std0 = zeros(N_cities,1);
+table44.mu1 = zeros(N_cities,1);
+table44.std1 = zeros(N_cities,1);
+table44.cer = zeros(N_cities,1);
+table44_mvgamma{20} = table44.cer;
+%}
+
+%{
+% want to be able to get rid of thse soon
+N_pred = 14;            % number of predictor variables w/ microdata
+N_pred0 = N_pred - 2;   % number of predictor variables w/o microdata
+N_naive = 2;            % number naive forecasts
+N_combo = 5;            % number combination forecasts
+N_tot = N_pred + N_naive + N_combo; % number all forecasts w/microdata
+N_tot0 = N_tot - 2;
+%}
