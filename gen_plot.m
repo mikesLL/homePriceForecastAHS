780d88c14950912e4dfa2c2add18c5a782e807a5
Michael Sharifi <michael.a.sharifi@gmail.com>
2016-04-01 12:16:59 -0700

    adding figures and new code

--- gen_plot.m
+++ gen_plot.m
@@ -1,16 +1,17 @@
 function gen_plot(param, ds_use, city_id, city_str )
 
+addpath('figs');
 idx1 = all([ ds_use.city_id == city_id, ds_use.YEAR >= param.year_beg, ...
     ds_use.YEAR <= param.year_end] , 2 );
 
 figure; plot(ds_use.YEAR(idx1),ds_use.risk_idx(idx1));
-%title( strcat(city_str,': Negative Equity Homeowners') );
+print(strcat('figs/negeq_',char(lower(city_str))),'-dpng'); % Neg Eq homeowners
 
 figure; plot(ds_use.YEAR(idx1), ds_use.risk_idx2(idx1));
-%title( strcat(city_str,': Potential Buyers') );
+print(strcat('figs/pb_',char(lower(city_str))),'-dpng');  % Potential Buyers
 
 figure; plot(ds_use.YEAR(idx1), ds_use.PRICE(idx1));
-%title( strcat(city_str,': Price') );
+print(strcat('figs/price_',char(lower(city_str))),'-dpng'); % Prices
 
 
 end
--- main.m
+++ main.m
@@ -31,9 +31,11 @@ table4 = repmat(table4, 1, 5);
 table4.Properties.VarNames = {'mu0', 'std0', 'mu1', 'std1', 'cer' };
 
 %% print an example for Los Angeles
-city_id = 2;
-gen_plot(param, ds_use, city_id, dsreadin_codes.city_str(city_id));
+for city_id = 1:17
+    gen_plot(param, ds_use, city_id, dsreadin_codes.city_str(city_id));
+end
 
+%%
 util_diff = zeros(N_cities,1);
 util0_store = zeros(N_cities,1);
 util1_store = zeros(N_cities,1);
